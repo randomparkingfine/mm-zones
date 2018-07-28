fb5837f48bd4eeb75066a6c208c71ca6596d5ef3
shockrahwow <alejandros714@protonmail.com>
2018-07-27 22:10:12 -0700

    added: bhop_combine bhop_kz_mix_journeys, bhop_verisimilitude

--- zfix.py
+++ zfix.py
@@ -1,5 +1,5 @@
 import os,sys
-def startFix(file_Name):
+def start_Fix(file_Name):
     newLine = "		\"bhopleavespeed\"		\"290.000000\"\n"
     oldLine = "		\"bhopleavespeed\"		\"250.000000\""
     swap = []
@@ -16,12 +16,20 @@ def startFix(file_Name):
     
     # fallthrough
     print("Success: {}".format(file_Name))
-    
+
+def to_Fix(target):
+    oldLine = "		\"bhopleavespeed\"		\"250.000000\""
+    with open(target, 'r') as tData:
+        for line in tData:
+            if oldLine in line:
+                return True
+    return False
+
 def file_Grab(directory):
     for file_ in os.listdir(directory):
-        if file_.endswith(".zon"):
+        if file_.endswith(".zon") and to_Fix(directory + '/' + file_):
             print("Found file_: {}".format(file_))
-            startFix(directory + '/' + file_)
+            start_Fix(directory + '/' + file_)
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
